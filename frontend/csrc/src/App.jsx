3f3351d765da96e1aeb8e66cadb894eaf50ea301
DARSHAN-V-G <darshanguna22@gmail.com>
2025-05-05 08:41:44 +0530

    changed browserRoute to HashRouter

--- frontend/csrc/src/App.jsx
+++ frontend/csrc/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/authContext.jsx';
 import CreateReportPage from './pages/CreatReport.jsx';
 import LoginPage from './pages/LoginPage.jsx';
