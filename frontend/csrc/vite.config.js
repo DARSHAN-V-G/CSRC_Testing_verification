f35a5890cd11babf52df6993477954b82e72baba
DARSHAN-V-G <darshanguna22@gmail.com>
2025-05-05 09:06:45 +0530

    modified docker cmd and vite config

--- frontend/csrc/vite.config.js
+++ frontend/csrc/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/testing/', // This tells Vite to prepend /testing/ to all asset URLs
+  base: '/testing/',
+  server: {
+    host: true
+  },
+ // This tells Vite to prepend /testing/ to all asset URLs
 })
